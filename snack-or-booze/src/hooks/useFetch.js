41836a481ba1ea1bc7c2e66970d70bf702b10a42
angel <angelquezada554@gmail.com>
2023-11-14 11:05:26 -0500

    apiNew

--- snack-or-booze/src/hooks/useFetch.js
+++ snack-or-booze/src/hooks/useFetch.js
@@ -1,7 +1,39 @@
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+
+// const BASE_API_URL = "http://localhost:5000";
+
+
+// const useFetch = (endpoint) => {
+//   const [data, setData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const result = await axios.get(`${BASE_API_URL}/${endpoint}`);
+//         setData(result.data);
+//         setLoading(false);
+//       } catch (error) {
+//         setError(error);
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchData();
+//     console.log(data)
+//   }, [endpoint]);
+
+//   return { data, loading, error };
+// };
+
+// export default useFetch;
+
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const BASE_API_URL = "http://localhost:5000";
+const BASE_API_URL = "https://spanglish-back-end.onrender.com";
 
 
 const useFetch = (endpoint) => {
@@ -29,3 +61,4 @@ const useFetch = (endpoint) => {
 };
 
 export default useFetch;
+
